9d53e889760f100bc7d75616d623136b3f4e96d6
Diana Harlap <dianaharlap004@gmail.com>
2023-10-23 14:48:04 +0300

    Fix bugs

--- src/main/java/by/harlap/stream/task/impl/Task7.java
+++ src/main/java/by/harlap/stream/task/impl/Task7.java
@@ -1,9 +1,11 @@
 package by.harlap.stream.task.impl;
 
+import by.harlap.stream.collector.CustomListCollector;
 import by.harlap.stream.model.Person;
 import by.harlap.stream.task.Task;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Task7 implements Task {
     private final List<Person> persons;
@@ -17,13 +19,17 @@ public class Task7 implements Task {
     @Override
     public void run() {
         System.out.printf("\n7.Получите список Person и проверьте есть ли в этом списке человек, у которого номер телефона %s значению.\n\n", phoneNumber);
-
-        boolean personWithPhoneNumberExists = persons.stream()
-                .anyMatch(person -> person.phones()
+        List<Person> personWithPhoneNumber = persons.stream()
+                .filter(person -> person.phones()
                         .stream()
-                        .anyMatch(phone -> phone.number().equals(phoneNumber)));
-        if (personWithPhoneNumberExists) {
+                        .anyMatch(phone -> phone.number().equals(phoneNumber)))
+                .collect(CustomListCollector.toList());
+
+        if (personWithPhoneNumber != null) {
             System.out.println("Человек с номером телефона " + phoneNumber + " есть в списке.");
+            for (Person person : personWithPhoneNumber) {
+                System.out.println(person);
+            }
         } else {
             System.out.println("Человека с номером телефона " + phoneNumber + " нет в списке.");
         }
